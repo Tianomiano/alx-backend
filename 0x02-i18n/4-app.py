982aa23af3cb34406f4dc55b286abb0e650028f7
Tianomiano <patomiano6@gmail.com>
2023-10-31 14:56:02 +0300

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -44,7 +44,7 @@ def home() -> str:
     simply outputs “Welcome to Holberton” as page
     title (<title>) and “Hello world” as header (<h1>).
     """
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
