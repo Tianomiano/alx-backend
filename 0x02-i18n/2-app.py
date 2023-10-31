48957c985d4ebd2a44199c176d93d21c920b1e51
Tianomiano <patomiano6@gmail.com>
2023-10-31 13:54:25 +0300

    get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -34,6 +34,9 @@ def home():
 
 @babel.localeselector
 def get_locale():
+    """
+    determine the best match with our supported languages.
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
